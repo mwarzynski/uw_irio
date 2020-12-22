1fc7cf565ae7df61c1627a0370c73f46ceaff448
Mateusz Warzynski <mathew.warzynski@gmail.com>
2020-12-22 02:28:17 +0100

    infrastructure: Enable provisioning GAE for new applies

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -13,11 +13,11 @@ locals {
 
 // Cloud Firestore should be provisioned using... App Engine.
 // https://firebase.google.com/docs/firestore/solutions/automate-database-create#create_a_database_with_terraform
-//resource "google_app_engine_application" "app" {
-//  project     = var.project
-//  location_id = var.location
-//  database_type = "CLOUD_FIRESTORE"
-//}
+resource "google_app_engine_application" "app" {
+  project     = var.project
+  location_id = var.location
+  database_type = "CLOUD_FIRESTORE"
+}
 
 resource "google_storage_bucket" "bucket" {
   name = "${var.project}-tf-gcf"
