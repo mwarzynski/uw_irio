5efef8f77729540a9c62ddee64d52fccea3ea4ae
Mateusz Warzynski <mathew.warzynski@gmail.com>
2020-12-22 02:10:02 +0100

    frontend-api: Fix the 'publishedDate' format

--- components/frontend-api/main.py
+++ components/frontend-api/main.py
@@ -7,5 +7,13 @@ db = firestore.Client()
 
 def main(request):
     news_ref = db.collection(u'news')
-    news = news_ref.stream()
-    return jsonify({"news": [n.to_dict() for n in news]})
+    news = []
+    for n in news_ref.stream():
+        item = n.to_dict()
+
+        # Improve the 'published date' format.
+        item['publishedDate'] = item['publishedDate'].strftime('%Y-%m-%dT%H:%M:%S.%fZ')
+
+        news.append(item)
+
+    return jsonify({"news": news})
