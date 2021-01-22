cbd7a7d0eaad50a90f63b2bf188b74fd11d095f9
Hubert Jasudowicz <hubert.jasudowicz@gmail.com>
2021-01-23 00:19:50 +0100

    Cleanup upload module (#13)

--- infrastructure/gcf/upload/main.tf
+++ infrastructure/gcf/upload/main.tf
@@ -1,12 +1,16 @@
+locals {
+  zip_file = "/tmp/${var.project}-gcf-${var.function_name}.zip"
+}
+
 data "archive_file" "source_code" {
   type        = "zip"
-  output_path = "/tmp/${var.project}-gcf-${var.function_name}.zip"
+  output_path = local.zip_file
   source_dir = "../components/${var.source_dir}"
 }
 
 resource "google_storage_bucket_object" "archive" {
-  name   = "${var.function_name}-${filemd5("/tmp/${var.project}-gcf-${var.function_name}.zip")}.zip"
+  name   = "${var.function_name}-${filemd5(local.zip_file)}.zip"
   bucket = var.bucket
-  source = "/tmp/${var.project}-gcf-${var.function_name}.zip"
+  source = local.zip_file
   depends_on = [data.archive_file.source_code]
 }
