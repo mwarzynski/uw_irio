24b97a171a8a695f46fc9d78806d53fc62f3cb41
Hubert Jasudowicz <hubert.jasudowicz@cert.pl>
2021-01-23 00:14:08 +0100

    Add a public static file bucket

---
+++ infrastructure/static.tf
@@ -0,0 +1,32 @@
+locals {
+  project_root = abspath(join("/", [path.root, ".."]))
+  build_dir    = join("/", [local.project_root, "components/frontend/build"])
+  static_files = tolist(fileset(local.build_dir, "**"))
+}
+
+# Bucket to store static objects
+resource "google_storage_bucket" "static-files" {
+  name = "${var.project}-static-files"
+  # FIXME
+  location = "US"
+
+  website {
+    main_page_suffix = "index.html"
+    not_found_page   = "404.html"
+  }
+}
+
+# Everyone can read
+resource "google_storage_default_object_access_control" "public_rule" {
+  bucket = google_storage_bucket.static-files.name
+  role   = "READER"
+  entity = "allUsers"
+}
+
+# Iterate over all files in static file list and put them into bucket
+resource "google_storage_bucket_object" "frontend-static" {
+  count  = length(local.static_files)
+  name   = local.static_files[count.index]
+  source = join("/", [local.build_dir, local.static_files[count.index]])
+  bucket = google_storage_bucket.static-files.name
+}
