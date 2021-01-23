39512ad331a4556b4236731f7e15432fff402d2f
Hubert Jasudowicz <hubert.jasudowicz@cert.pl>
2021-01-23 14:52:21 +0100

    Cache news data in frontend API

--- components/frontend-api/main.py
+++ components/frontend-api/main.py
@@ -1,11 +1,15 @@
+import os
+from cachetools import cached, TTLCache
 from flask import jsonify
 from google.cloud import firestore
 
 # GCLOUD_PROJECT
 db = firestore.Client()
 
+CACHE_TTL = int(os.getenv("CACHE_TTL", 60))
 
-def main(request):
+@cached(cache=TTLCache(maxsize=1, ttl=CACHE_TTL))
+def get_news():
     news_stream = db.collection(u'news').order_by(u'publishedDate', direction=firestore.Query.DESCENDING).limit(20).stream()
     news = []
     for n in news_stream:
@@ -13,9 +17,12 @@ def main(request):
         # Improve the 'published date' format.
         item['publishedDate'] = item['publishedDate'].strftime('%Y-%m-%dT%H:%M:%S.%fZ')
         news.append(item)
+    return news
 
+
+def main(request):
     headers = {
         'Access-Control-Allow-Origin': '*'
     }
 
-    return (jsonify({"news": news}), 200, headers)
+    return (jsonify({"news": get_news()}), 200, headers)
